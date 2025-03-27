6de5fad218ad13c2100fdfe11e9d88f1039a685b
yejin0117 <yejin5480@naver.com>
2025-03-27 16:10:44 +0900

    4주차 0327 16:10

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import MyB from "./MyButton.js"
+import { Button1,Button3 } from "./ButtonLib.js"
+import AP from "./AboutPage.js"
+import ProF from "./Profile.js"
+
 //return할 때 값이 여러개면 랩핑 필요
 export default function App() {
   return(
     <div>
       <h1>Hello React</h1>
-      <MyB/>
+      <MyB/> <br/>
+      <Button1/> &nbsp;
+      <Button3/>
+      <AP/>
+      <ProF/>
     </div>
   )
 }
---
+++ src/ButtonLib.js
@@ -0,0 +1,19 @@
+function Button1(){
+  return(
+    <button>Button1</button>
+  )
+}
+
+function Button2(){
+  return(
+    <button>Button2</button>
+  )
+}
+
+function Button3(){
+  return(
+    <button>Button3</button>
+  )
+}
+
+export { Button1, Button2, Button3 }
